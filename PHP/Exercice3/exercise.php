c6d938d19d992eab24753d75c6d24771a93a7182
mfdaniel <danielmartafernandes@gmail.com>
2018-02-27 15:55:47 +0100

    php_exer_3

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,20 @@
 <?php
 
-$input;
-$winner;
+$aResult = 0;
+$bResult = 0;
+
+foreach($input as $time) {
+    list($a, $b) = $time;
+    
+    if ($a > $b) {
+        $aResult++;
+    } else if ($b > $a) {
+        $bResult++;
+    }
+}
+
+if ($aResult > $bResult) {
+    $winner = 'A';
+} else {
+    $winner = 'B';
+}
