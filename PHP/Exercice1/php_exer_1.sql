a42bf8abed5602c8c33d9af4c8d34c0a171d5381
mfdaniel <danielmartafernandes@gmail.com>
2018-02-27 11:05:45 +0100

    php_exer_1.sql

---
+++ PHP/Exercice1/php_exer_1.sql
@@ -0,0 +1,23 @@
+{\rtf1}
+<?php
+
+$booleanTrue = true;
+$booleanFalse = false;
+
+$int = 5;
+
+$float = 1.5;
+
+$integer = -12;
+
+$double = 1.3;
+
+$string = "hi";
+
+$array=[12,"hello"];
+
+$doubleDimenssionArray=[    'panama' => [9.0817275, -79.5932209],    'luxembourg' => [49.6076553, 6.1008508]] ;
+
+$associativeArray=[9.0817275, -79.5932209];
+
+$null= null;
