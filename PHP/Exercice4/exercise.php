246a2d261fd6963c7903598e4778f2d15147d3b9
mfdaniel <danielmartafernandes@gmail.com>
2018-02-28 11:15:29 +0100

    PHP_exercise_4

--- PHP/Exercice4/exercise.php
+++ PHP/Exercice4/exercise.php
@@ -1,1 +1,19 @@
 <?php
+function getAllMondaysOfMonth($year,$month) { 
+    $mondays = [];
+    
+    $currentMonth = DateTime::createFromFormat('Ym', $year . $month);
+    $firstMonday = new DateTime('first monday of'.$currentMonth->format('F Y'));
+    
+    $interval = DateInterval::createFromDateString('next monday');
+    
+    $lastMonday = new DateTime('last monday of'.$currentMonth->format('F Y'));
+    while($firstMonday<=$lastMonday){
+        $mondays[]= $firstMonday->format('l j, M Y');
+        $firstMonday->add($interval);
+    }
+    return $mondays;
+}
+
+
+
